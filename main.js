90d8f64071b4149fb9f16460d8b07b9fd82833d6
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2024-10-14 14:38:11 +0200

    Added ul for displaying list

--- main.js
+++ main.js
@@ -11,15 +11,23 @@ const database = firebase.database(app);
 
 const cartList = database.ref('cartList');
 
-console.log(app);
 
 const addToCartButton = document.getElementById('addButton');
 const itemInput = document.getElementById('item');
+const shoppingList = document.querySelector('.shopping-list');
 
 
 addToCartButton.addEventListener('click', (event) => {
     event.preventDefault();
   let item = itemInput.value;
   cartList.push(item);
+  const listItem = document.createElement('li');
+  listItem.textContent = item; // Set the text of the li to the item
+
+  // Append the li to the ul
+  shoppingList.appendChild(listItem);
+
+  // Clear the input field after adding the item
+  itemInput.value = '';
   console.log('Adding item:', item);
 });
