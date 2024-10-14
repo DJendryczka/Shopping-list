bb6078c10e60031d365474acf1b309d82911ba69
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2024-10-14 14:13:08 +0200

    Connected firebase to app OK

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+
+
+const firebaseConfig = {
+  databaseURL: "https://shoppinglist-c9fec-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database(app);
+
+const cartList = database.ref('cartList');
+
+console.log(app);
+
 const addToCartButton = document.getElementById('addButton');
 const itemInput = document.getElementById('item');
 
@@ -5,5 +20,6 @@ const itemInput = document.getElementById('item');
 addToCartButton.addEventListener('click', (event) => {
     event.preventDefault();
   let item = itemInput.value;
+  cartList.push(item);
   console.log('Adding item:', item);
 });
